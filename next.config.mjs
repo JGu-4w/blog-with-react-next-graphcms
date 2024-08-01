5c3a44bb79942d61bba6e70c4db94f92d1729690
Joe Gu <joegu.4work@gmail.com>
2024-08-01 17:20:07 +0800

    feat: finish PostDetail and Author

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ap-northeast-1.graphassets.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
