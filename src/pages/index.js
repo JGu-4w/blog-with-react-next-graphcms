d61cce1f78ffe8f86b5e1083c6c06d56826d8102
Joe Gu <joegu.4work@gmail.com>
2024-08-01 22:59:51 +0800

    feat: add FeaturedPost

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import Head from 'next/head';
 
 import { PostCard, Categories, PostWidget } from '@/components';
+import { FeaturedPosts } from '@/sections';
 import { getPosts } from '@/services';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,6 +14,7 @@ export default function Home({ posts }) {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
